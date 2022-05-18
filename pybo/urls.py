914eb509c435497587a0345db2acc56f60253ae4
randomkew <ta5861@naver.com>
2022-05-18 10:00:59 +0900

    파이보

--- pybo/urls.py
+++ pybo/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
 
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 
-from ..models import Question
+from ..models import Question, Answer
 
 
 @login_required(login_url='common:login')
@@ -16,3 +16,16 @@ def vote_question(request, question_id):
     else:
         question.voter.add(request.user)
     return redirect('pybo:detail', question_id=question.id)
+
+
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    """
+    pybo 답글추천등록
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id)
